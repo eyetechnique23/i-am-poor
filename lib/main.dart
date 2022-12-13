2ae01b6417aa95c00766d6c60a2bde94511d180a
eyetechnique23 <114220680+eyetechnique23@users.noreply.github.com>
2022-12-13 19:45:27 +0800

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text('I am Poor by Jayson Mamanta',
+              style: TextStyle(color: Colors.black)),
+          backgroundColor: Colors.white10,
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/py.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
